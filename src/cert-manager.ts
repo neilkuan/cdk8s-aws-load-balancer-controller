3151e0e6f436cf5731ee8a032f1bc6e3367391ff
Neil Kuan <guan840912@gmail.com>
2020-11-03 17:44:39 +0800

    fix(cert): fix cert path not found

--- src/cert-manager.ts
+++ src/cert-manager.ts
@@ -1,8 +1,9 @@
 import * as fs from 'fs';
+import * as path from 'path';
 import * as yaml from 'js-yaml';
 export class CertManager {
   public static certManagerConfig(): any {
-    let certManagerConfigManifest = yaml.safeLoadAll(fs.readFileSync('../cert/cert-manager.yaml').toString());
+    let certManagerConfigManifest = yaml.safeLoadAll(fs.readFileSync(path.join(__dirname, '../cert/cert-manager.yaml')).toString());
     return certManagerConfigManifest;
   }
 }
